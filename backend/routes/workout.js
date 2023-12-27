eab0c4a7cf4110c84997cd4e121bc258e8e5acb5
Jonathan Yan <jonathan.yan10@googlemail.com>
2023-12-27 17:07:49 +0000

    Server, Express middleware and Route set up

---
+++ backend/routes/workout.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+// Create an instance of the router
+const router = express.Router();
+
+// Get all workouts
+router.get("/", (req, res) => {
+  res.json({ mssg: "Get all workouts" });
+});
+
+// Get single workouts
+router.get("/:id", (req, res) => {
+    res.json({ mssg: "Get a single workout" });
+});
+
+// Post a new workouts
+router.post("/", (req, res) => {
+    res.json({ mssg: "Post a new workout" });
+});
+
+// Delete a workouts
+router.delete("/:id", (req, res) => {
+    res.json({ mssg: "Delete a workout" });
+});
+
+// Update a workout
+router.patch("/:id", (req, res) => {
+    res.json({ mssg: "Update a workout" });
+});
+
+module.exports = router;
