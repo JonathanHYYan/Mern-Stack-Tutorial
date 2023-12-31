f7aac6d6025191d2bed8f6c3007430a10b1f546d
Jonathan Yan <jonathan.yan10@googlemail.com>
2023-12-31 13:54:37 +0000

    Added login and signup setup in backend

---
+++ backend/controller/userController.js
@@ -0,0 +1,13 @@
+const User = require("../models/userModel");
+
+// Login User
+const loginUser = async (req, res) => {
+  res.json({ mssg: "Login User" });
+};
+
+// Signup User
+const signupUser = async (req, res) => {
+  res.json({ mssg: "Signup User" });
+};
+
+module.exports = { signupUser, loginUser };
