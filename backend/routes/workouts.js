2b8018294051c3686624ecf738b7c9cc4dfb4938
Jonathan Yan <jonathan.yan10@googlemail.com>
2024-08-26 18:14:29 +0100

    Added request token authorisation for api routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { getWorkouts, getWorkout, createWorkout, deleteWorkout, updateWorkout } = require("../controller/workoutController");
+const requireAuth = require('../middleware/requireAuth');
 
 // Create an instance of the router
 const router = express.Router();
+
+// Require auth for all workout routes
+router.use(requireAuth);
 
 // Get all workouts
 router.get("/", getWorkouts);
