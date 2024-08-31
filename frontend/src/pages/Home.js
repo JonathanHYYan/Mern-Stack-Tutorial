3ef95c2d9968ace06d57a78b3dee46aac4e313ec
Jonathan Yan <jonathan.yan10@googlemail.com>
2024-08-31 15:58:57 +0100

    Added auth requests

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,35 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // Components
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const {workouts, dispatch} = useWorkoutsContext();
+  const { workouts, dispatch } = useWorkoutsContext();
+  const {user} = useAuthContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       // The lack of URL here means its going to the package.json "{proxy:}" When live and has domain we might need to do something here.
       // In prod every request will need to point to the correct end points
-      const response = await fetch("/api/workouts");
+      const response = await fetch("/api/workouts", {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      });
       const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_WORKOUTS', payload: json});
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
     };
 
-    fetchWorkouts();
-  }, [dispatch]);
+    if(user){
+      fetchWorkouts();
+    }
+  }, [dispatch, user]);
 
   return (
     <div className="home">
